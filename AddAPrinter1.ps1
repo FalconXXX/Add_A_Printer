4d886bbdc1d1c0343e473bc5ee7de981f466eec9
FalconXXX <39584680+FalconXXX@users.noreply.github.com>
2021-07-30 20:13:28 +0200

    ReadMe

--- AddAPrinter1.ps1
+++ AddAPrinter1.ps1
@@ -1,4 +1,5 @@
 #run as Admin
+#Saving printers from the print server 
 #add the path ;)	
 
 Get-WmiObject -Class Win32_Printer -ComputerName printserver | select Name, PortName, DriverName | Export-Csv -Path \\Path\Drucker.csv -NoClobber -Delimiter ","
