e1c8ccbddf8dbf2ab30871d3766bb65ceccc997d
FalconXXX <39584680+FalconXXX@users.noreply.github.com>
2022-04-27 10:57:11 +0200

    Update AddAPrinter2.ps1

--- AddAPrinter2.ps1
+++ AddAPrinter2.ps1
@@ -2,7 +2,7 @@
 #           add a printer \ *<*/  (for Users)               #
 #           11.05.2019 					    #
 #	    by FalcoonXXX 		 		    #
-#           Build & Tested on:       Windows 10   	    # 
+#           Build & Tested on:       Windows 10   & 11	    # 
 #	    https://github.com/FalconXXX/PowerShell.git     #
 #	    add the path ;)                                 #
 #===========================================================#
